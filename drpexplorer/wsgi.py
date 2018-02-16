6865f33ee05837bfb3b418919148c85b809100b5
Nicoals Chotard <nchotard@in2p3.fr>
2018-02-16 05:13:21 +0100

    Change name + cleaning

--- drpexplorer/wsgi.py
+++ drpexplorer/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for lsstdre project.
+WSGI config for drpexplorer project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lsstdre.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "drpexplorer.settings")
 
 application = get_wsgi_application()
