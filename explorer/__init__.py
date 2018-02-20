fa7e5eae539356a1d4b826ff65ca09be3705b582
Nicoals Chotard <nchotard@in2p3.fr>
2018-02-20 10:06:13 +0100

    Add python libraries

---
+++ explorer/__init__.py
@@ -0,0 +1,20 @@
+import os
+import sys
+
+# django settings
+os.environ['DJANGO_SETTINGS_MODULE'] = 'drpexplorer/settings'
+
+# extract django when runing the first time
+django_version = 'Django-2.0.2'
+ppath = os.path.join(os.path.dirname(__file__), 'static/python/')
+if not os.path.exists(ppath+django_version):
+    import tarfile
+    t = tarfile.open(ppath+django_version+'.tgz')
+    t.extractall(path=ppath)
+    t.close()
+
+# add django and other library to the python path
+sys.path.insert(0, ppath+django_version)
+sys.path.insert(0, ppath)
+
+__all__ = ['explorer', 'butler', 'db']
