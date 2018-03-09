00c0e46ad1ab8aff1c5922be67cb65feb41275f7
nicolaschotard <nchotard@in2p3.fr>
2018-03-09 15:38:31 +0100

    Add js9 in the repo

--- drpexplorer/explorer/__init__.py
+++ drpexplorer/explorer/__init__.py
@@ -9,13 +9,27 @@ django_version = 'Django-2.0.2'
 ppath = os.path.join(os.path.dirname(__file__), 'static/python/')
 if not os.path.exists(ppath+django_version):
     import tarfile
-    t = tarfile.open(ppath+django_version+'.tgz')
+    t = tarfile.open(ppath + django_version+'.tgz')
     t.extractall(path=ppath)
     t.close()
 
 # add django and other library to the python path
 sys.path.insert(0, ppath+django_version)
 sys.path.insert(0, ppath)
+
+# extract js9 the first time we run
+js9_version = 'js9-2.0.2'
+spath = os.path.join(os.path.dirname(__file__), 'static/')
+if not os.path.exists(spath + js9_version):
+    # extract js9
+    import tarfile
+    t = tarfile.open(spath + js9_version+'.tar.gz')
+    t.extractall(path=spath)
+    t.close()
+    # create a sym link to this version of js9
+    if os.path.islink(spath + 'js9'):
+        os.remove(spath + 'js9')
+    os.symlink(spath + js9_version, spath + 'js9')
 
 # Create a links directory the first time we run
 links_path = os.path.join(os.path.dirname(__file__), 'links')
